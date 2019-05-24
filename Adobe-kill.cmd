8a8c035aa81a42eded98a67f83c20e49e4874647
emilsvfx <emilsvfx@gmail.com>
2019-05-24 09:23:55 +0300

    Add files via upload

---
+++ Adobe-kill.cmd
@@ -0,0 +1,15 @@
+TASKKILL /F /IM CCLibrary.exe
+TASKKILL /F /IM AdobeIPCBroker.exe
+TASKKILL /F /IM AGMService.exe
+TASKKILL /F /IM AGMService.exe
+TASKKILL /F /IM AdobeUpdateService.exe
+TASKKILL /F /IM armsvc.exe
+TASKKILL /F /IM CoreSync.exe
+TASKKILL /F /IM Adobe Desktop Process.exe
+TASKKILL /F /IM Adobe Installer.exe
+TASKKILL /F /IM CCXProcess.exe 
+TASKKILL /F /IM AdobeGCClient.exe 
+TASKKILL /F /IM AGSService.exe 
+TASKKILL /F /IM Adobe CEF Helper.exe
+TASKKILL /F /IM Creative Cloud.exe
+TASKKILL /F /IM Adobe Installer.exe
